0a89a7ed210f0fdc5311d292baac82a1e7abd275
Lincoln dos Reis Elias <lincolnreistkd@gmail.com>
2022-06-25 12:49:52 -0300

    tasks por hora

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #precisa ser feito antes to import do pyspark
+from xmlrpc.client import MAXINT
 import findspark
 findspark.init()
 #-------------------------------------------#
@@ -6,18 +7,22 @@ import pyspark
 from pyspark_dist_explore import hist
 import time
 import matplotlib.pyplot as plt
+from pyspark.sql.functions import col
+import math
+import sys
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType,StructField, StringType, IntegerType,FloatType
+from pyspark.sql.functions import udf
+from pyspark.sql.types import StructType,StructField, LongType, IntegerType,FloatType
 spark = SparkSession.builder.appName("Sessao").getOrCreate()
 sc = spark.sparkContext
 instance_events_schema = StructType([ \
-    StructField("time",IntegerType(),True), \
+    StructField("time",LongType(),True), \
     StructField("type",IntegerType(),True), \
     StructField("collection_id",IntegerType(),False), \
     StructField("priority", IntegerType(), True), \
     StructField("instance_index", IntegerType(), False), \
-    StructField("cpu_resource_request", FloatType(), True), \
-    StructField("memory_resource_request", FloatType(), True) \
+    StructField("resource_request.cpus", FloatType(), True), \
+    StructField("resource_request.memory", FloatType(), True) \
   ])
 #carrega o csv em um RDD, sem cabeçalho
 rddCE = spark.read.option("header","true").schema(instance_events_schema).csv("instance_events/instance_events-000000000000.csv")
@@ -27,4 +32,15 @@ rddCE = spark.read.option("header","true").schema(instance_events_schema).csv("i
 
 #fig, ax = plt.subplots()
 #test = hist(ax, rddCE.select('type'), bins = 20, color=['red'])
-rddCE.orderBy('type').groupBy('type').agg({"`memory_resource_request`":'avg'}).show()
+rddCE.persist()
+#rddCE.orderBy('time').groupBy('time').agg({"`memory_resource_request`":'avg'}).show()
+
+rddCE = rddCE.filter((rddCE.time.isNotNull()) & (rddCE.type==3) & (rddCE.time>0) & (rddCE.time<sys.maxsize))
+def microToHour(x):
+    return math.floor(x/3.6e+9)
+rddCE.take(5)
+rddCE.withColumn("hour",int(col("time"))).select("time","hour").distinct().show(5)
+udf_hours = udf(lambda x:microToHour(x),IntegerType())
+
+rddCE.withColumn("hour",udf_hours(col("time"))).show(10)
+rddCE.withColumn("hour",udf_hours(col("time"))).select('time','hour').agg({'time','max'}).collect[0]
